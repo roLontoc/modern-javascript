1ea4ac074755a877987633070b6bc1708c27609a
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-08 09:30:08 -0500

    Added string usuage examples in sandbox

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,4 +1,8 @@
+//ctrl foward slash for shorthand comment
+//internet 11 does not support modern javascript
+
 //variables
+
 //Naming conventions: camelCase, no spaces, cannot start with a number, let and const cannot be used (reserved keywords)
 // let is the new way of writing variables
 let age = 25;
@@ -17,5 +21,36 @@ console.log(points);
 var score = 75;
 console.log(score);
 
-//ctrl foward slash for shorthand comment
-//internet 11 does not support modern javascript
+
+
+//strings
+
+//single or double quotes to make it a string
+console.log("hello world");
+let email = "rlontoc39@academic.rrc.ca"
+console.log(email);
+
+//string concatenation
+let firstName = "Brandon";
+let lastName = "Sanderson";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+//these methods do not alter the original value
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+//value passed into method called a arguement or parameter
+let index = email.indexOf("@");
+console.log(index);
