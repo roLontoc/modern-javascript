0d1a7f2942b11b7ceff16671721502e36a4fe45e
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-08 13:14:44 -0500

    added example usage of logical operators to sandbox.js

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -49,26 +49,48 @@
 const age = 25;
 
 // message will be shown
-if(age > 20){
-  console.log("you are over the age of 20.");
-}
+// if(age > 20){
+//   console.log("you are over the age of 20.");
+// }
 
-const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
 
-// message we not be shown
-if(ninjas.length > 4) {
-  console.log("that's a lot of ninjas")
-}
+// // message we not be shown
+// if(ninjas.length > 4) {
+//   console.log("that's a lot of ninjas")
+// }
 
-const password = "passwordsarecool";
+// const password = "passwordsarecool";
 
-if(password.length >=12){
-  console.log("that password is mighty strong")
-}
-else if(password.length >= 8){
-  console.log("the password is long enough!");
-}
-else {
-  console.log("your password is not long enough.")
+// if(password.length >=12){
+//   console.log("that password is mighty strong")
+// }
+// else if(password.length >= 8){
+//   console.log("the password is long enough!");
+// }
+// else {
+//   console.log("your password is not long enough.")
+// }
+
+// // LOGICAL OPERATORS
+// const password = "p@ss";
+
+// if(password.length >=12 && password.includes("@")){
+//   console.log("that password is mighty strong")
+// }
+// else if(password.length >= 8 || password.includes("@") && password.length > 5){
+//   console.log("the password is strong enough!");
+// }
+// else {
+//   console.log("your password is not strong enough.")
+// }
+
+
+// // LOGICAL NOT (!)
+
+let user = false;
+
+if(!user){
+  console.log("you must be logged in to continue");
 }
 
