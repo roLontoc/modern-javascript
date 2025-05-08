b0eb2965842c4495c0a7e2c05e2290b06160d4d5
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-08 11:46:13 -0500

    added example usuage of while and do while loops in sandbox.js

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -6,12 +6,39 @@
 // console.log("loop done");
 
 // looping through array
-const names = ["shaun", "mario", "luigi"];
+// const names = ["shaun", "mario", "luigi"];
 
-for(let i = 0; i < names.length; i++){
-  //console.log(names[i]);
-  let html = `<div>${names[i]}</div>`
-  console.log(html);
-}
+// for(let i = 0; i < names.length; i++){
+//   //console.log(names[i]);
+//   let html = `<div>${names[i]}</div>`
+//   console.log(html);
+// }
 
 
+
+// // WHILE LOOPS
+// let i = 0;
+
+// // this would be an infinate loop without the i++
+// while(i < 5){
+//   console.log("in loop:", i)
+//   i++; //added to break that infinate loop
+// }
+
+// const names = ["shaun", "mario", "luigi"];
+
+// let i = 0;
+// while(i < names.length){
+//   console.log(names[i]);
+//   i++
+// }
+
+
+
+// // DO WHILE LOOPS
+
+let i = 3;
+do{
+  console.log("val of i is:", i)
+  i++;
+} while(i <5);
