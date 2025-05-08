8b10334cf29ed9bfafed2874a6d26e1b1dbe0a0a
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-08 11:36:15 -0500

    added example usage of for loops to sandbox.js

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,17 @@
+// // FOR LOOPS
+// for(let i = 0; i < 5; i++){
+//   console.log("in loop:", i);
+// }
+
+// console.log("loop done");
+
+// looping through array
+const names = ["shaun", "mario", "luigi"];
+
+for(let i = 0; i < names.length; i++){
+  //console.log(names[i]);
+  let html = `<div>${names[i]}</div>`
+  console.log(html);
+}
+
+
