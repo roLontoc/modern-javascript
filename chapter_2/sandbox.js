ae7e0cd062a3cb643c66c0d40610771e555e6210
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-08 09:13:44 -0500

    Coded examples of variables in sandbox.js

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,21 @@
+//variables
+//Naming conventions: camelCase, no spaces, cannot start with a number, let and const cannot be used (reserved keywords)
+// let is the new way of writing variables
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+//const is used for variables that will not change
+const points = 100;
+console.log(points);
+
+//old way of delcaring variables
+var score = 75;
+console.log(score);
+
+//ctrl foward slash for shorthand comment
+//internet 11 does not support modern javascript
