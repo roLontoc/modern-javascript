7d60075873ef8c6b50628aad2fe124d384b6ec9d
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-08 09:39:58 -0500

    Added more examples of methods that can be used with string

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,56 +1,77 @@
-//ctrl foward slash for shorthand comment
-//internet 11 does not support modern javascript
+// //ctrl foward slash for shorthand comment
+// //internet 11 does not support modern javascript
 
-//variables
+// //variables
 
-//Naming conventions: camelCase, no spaces, cannot start with a number, let and const cannot be used (reserved keywords)
-// let is the new way of writing variables
-let age = 25;
-let year = 2019;
+// //Naming conventions: camelCase, no spaces, cannot start with a number, let and const cannot be used (reserved keywords)
+// // let is the new way of writing variables
+// let age = 25;
+// let year = 2019;
 
-console.log(age, year);
+// console.log(age, year);
 
-age = 30;
-console.log(age);
+// age = 30;
+// console.log(age);
 
-//const is used for variables that will not change
-const points = 100;
-console.log(points);
+// //const is used for variables that will not change
+// const points = 100;
+// console.log(points);
 
-//old way of delcaring variables
-var score = 75;
-console.log(score);
+// //old way of delcaring variables
+// var score = 75;
+// console.log(score);
 
 
 
-//strings
+// //strings
 
-//single or double quotes to make it a string
-console.log("hello world");
-let email = "rlontoc39@academic.rrc.ca"
-console.log(email);
+// //single or double quotes to make it a string
+// console.log("hello world");
+// let email = "rlontoc39@academic.rrc.ca"
+// console.log(email);
 
-//string concatenation
-let firstName = "Brandon";
-let lastName = "Sanderson";
+// //string concatenation
+// let firstName = "Brandon";
+// let lastName = "Sanderson";
 
-let fullName = firstName + " " + lastName;
+// let fullName = firstName + " " + lastName;
 
-console.log(fullName);
+// console.log(fullName);
 
-//getting characters
-console.log(fullName[0]);
+// //getting characters
+// console.log(fullName[0]);
 
-//string length
-console.log(fullName.length);
+// //string length
+// console.log(fullName.length);
 
-//string methods
-//these methods do not alter the original value
-console.log(fullName.toUpperCase());
+// //string methods
+// //these methods do not alter the original value
+// console.log(fullName.toUpperCase());
 
-let result = fullName.toLowerCase();
-console.log(result, fullName);
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
 
-//value passed into method called a arguement or parameter
-let index = email.indexOf("@");
-console.log(index);
+// //value passed into method called a arguement or parameter
+// let index = email.indexOf("@");
+// console.log(index);
+
+//common string methods
+//let email = "mario@thenetninja.co.uk";
+
+//finds the postition of the last instance of character
+//let result = email.lastIndexOf("n");
+
+//first arguement where we want to slice from, second where you want to slice to
+//let result = email.slice(0,10);
+
+//makes a substring from full string.
+// first arguement where you want to start from, second how many characters you want to get
+//let result = email.substr(4,10);
+
+//let result = email.replace("m", "w");
+
+//replaces the first character it comes across
+//let result = email.replace("n", "w");
+
+//console.log(result);
+
