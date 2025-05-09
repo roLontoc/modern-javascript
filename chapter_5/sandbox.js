1b5941cca0c52cc78b1b0806a143b36c338e1abc
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-09 11:50:03 -0500

    added example usage of object literals and using methods on object literals

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,1 +1,59 @@
 // // OBJECT LITERALS
+
+// put property on seperate line for readability
+// let user = {
+//   name: "crystal",
+//   age: 30,
+//   email: "crystal@tehnetninja.co.uk",
+//   location: "berlin",
+//   blogs: ["why mac and cheese rules", "10 things to make wiht marmite"]
+// };
+
+// console.log(user);
+
+// // use fot notation to grab a specific property
+// console.log(user.name);
+
+// // update a property on an object
+// user.age = 35;
+// console.log(user.age);
+
+// // sqaure bracket notation
+// console.log(user["email"]);
+// user["name"] = "chun-li";
+// console.log(user["name"]);
+
+// // can be useful if we are passing a variable in
+// const key = "location"
+// console.log(user[key]);
+
+
+
+// ADDING METHODS TO USER OBJECTS
+let user = {
+  name: "crystal",
+  age: 30,
+  email: "crystal@tehnetninja.co.uk",
+  location: "berlin",
+  blogs: ["why mac and cheese rules", "10 things to make wiht marmite"],
+  login: function(){ // : function not necessary
+    console.log("the user is logged in");
+  },
+  logout(){
+    console.log("the user is logged out");
+  },
+  logBlogs(){ //can't use arrow function because of the this keyword.
+  // When we use the this object it doent change the value
+    console.log("the user has written the following blogs:");
+    this.blogs.forEach(blog => {
+      console.log(blog);
+    })
+  }
+};
+
+// calling the function
+user.login();
+user.logout();
+user.logBlogs();
+
+
