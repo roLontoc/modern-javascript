b47547a7ab15fa14b668f2028240a427ad828110
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-09 13:40:38 -0500

    added example usage for querySelector in sandbox.js

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,2 +1,12 @@
 // DOM (Document Object Model)
 // created by the browser when the html document loads.
+
+// grabbing the error class parargraph
+const para = document.querySelector('div.error');
+console.log(para);
+
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll(".error")
+console.log(errors);
+console.log(paras[0]);
+
