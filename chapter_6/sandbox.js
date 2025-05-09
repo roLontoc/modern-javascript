dfd23fc54595e83bece946e2552c5e1b18a0d158
Rogelyn <rlontoc39@academic.rrc.ca>
2025-05-09 13:49:21 -0500

    added usage examples for getElementBy methods insandbox.js

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -10,3 +10,18 @@ const errors = document.querySelectorAll(".error")
 console.log(errors);
 console.log(paras[0]);
 
+// GET AN ELEMEMT BY ID
+
+const title = document.getElementById("page-title");
+console.log(title);
+
+// GET ELEMENT BY CLASS NAME
+
+const allErrors = document.getElementsByClassName("error");
+console.log(allErrors);
+console.log(allErrors[0]);
+
+// GET ELEMENTS BY THEIR TAG NAME
+const ps = document.getElementsByTagName("p");
+console.log(ps);
+console.log(ps[0]);
